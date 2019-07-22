55eaf33c714f1899f1d9a451e70ac5dbcd099887
Stephen Madison <stephen.d.madison@gmail.com>
2019-07-22 15:22:29 -0500

    Working Code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,101 @@
-var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+// Example queryURL for Giphy API
+var gifs = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+
+
+function displayGif() {
+
+    var q = $(this).attr("data-name");
+    var api_key = "&api_key=nBAkbogRHgIsRY86uWJH4q0ViWa7dkF2";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?" + "q=" + q + api_key;
+
+    console.log(queryURL);
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        var results = response.data;
+        console.log(results);
+        for (var i = 0; i < results.length; i++) {
+
+            var spaceOut = $("<div class='col-md-4'>");
+
+
+            var defaultAnimatedSrc = results[i].images.fixed_height.url;
+            var staticSrc = results[i].images.fixed_height_still.url;
+            var gifDisplay = $("<img>");
+
+            gifDisplay.attr("src", staticSrc);
+            gifDisplay.addClass("netflixGiphy");
+            gifDisplay.attr("data-state", "still");
+            gifDisplay.attr("data-still", staticSrc);
+            gifDisplay.attr("data-animate", defaultAnimatedSrc);
+            spaceOut.append(gifDisplay);
+            $("#movies-view").prepend(spaceOut);
+        }
+    });
+
+}
 
 // Function for displaying movie data
 function renderButtons() {
 
-  // YOUR CODE GOES HERE
+    // YOUR CODE GOES HERE
 
-  $('#buttons-view').empty();
+    $('#buttons-view').empty();
 
-  for (var i = 0; i < movies.length; i++) {
+    // for (var i = 0; i < gifs.length; i++) {
 
-    var newButton = $('<button>').text(movies[i]);
+    //     
 
-    $('#buttons-view').append(newButton);
+    //     $('#buttons-view').append(newButton);
+
+    for (var i = 0; i < gifs.length; i++) {
+
+        var newButton = $('<button>').text(gifs[i]);
+        // Adds a class of movie to our button
+        newButton.addClass("gif");
+        // Added a data-attribute
+        newButton.attr("data-name", gifs[i]);
+        // Provided the initial button text
+        newButton.text(gifs[i]);
+        // Added the button to the buttons-view div
+        $("#buttons-view").append(newButton);
 
 
-  }
+
+
+    }
 }
 
-  // This function handles events where one button is clicked
-  $("#add-movie").on("click", function () {
+
+
+// This function handles events where one button is clicked
+$("#add-movie").on("click", function (event) {
 
     // YOUR CODE GOES HERE
 
 
     event.preventDefault();
 
-    var newMovie = $('#movie-input').val();
+    var newGif = $("#movie-input").val().trim();
 
-    movies.push(newMovie);
+    gifs.push(newGif);
 
     renderButtons();
-  });
-  
+});
 
-// Calling the renderButtons function to display the initial list of movies
+$(document).on("click", ".gif", displayGif);
+
+// Calling the renderButtons function to display the initial list of gifs
 renderButtons();
+
+
+
+
+
+
+
+
